ceda13f693731d25ee077eb14133832e9b69b324
Sarathballi <sarathballi45@gmail.com>
2026-06-06 11:51:58 +0530

    Initial commit

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
 export const connectDB=async()=> {
-    await mongoose.connect("mongodb+srv://sarathkumarballi_db_user:wWb7LjJSamMnA1oS@cluster0.ssxw8fx.mongodb.net/Expense")
+    await mongoose.connect("")
     .then(()=> {console.log("DB conneted")})
 }
